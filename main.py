f4ad058620c02364b5b4d85da83a82e5f6953b62
Rollsocke <r.kubitschek93@gmail.com>
2024-11-22 18:15:29 +0100

    sort things in groups

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
 
     while True:
@@ -16,9 +23,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        for updated in updatable:
+            updated.update(dt)
+        
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        
+        for drawn in drawable:
+            drawn.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
