54b27f8b66f8dbb4a3df034b8bd6280561682568
Rollsocke <r.kubitschek93@gmail.com>
2024-11-22 17:27:53 +0100

    added player

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import *
+from player import Player
+
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -12,6 +15,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 
---
+++ player.py
@@ -0,0 +1,18 @@
+from circleshape import *
+from constants import *
+from main import *
+
+class Player(CircleShape):
+    super().__init__(PLAYER_RADIUS)
+    rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
